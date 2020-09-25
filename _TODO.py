84d95c59a701385a560fffd059339797fd5ab493
Jack <hughesy.dev@gmail.com>
2020-09-25 13:27:28 +0100

    Updated TODO list. Removed completed items. Improved clarity/formatting

--- _TODO.py
+++ _TODO.py
@@ -1,17 +1,8 @@
-# for ID selection, names : drinks
-# ID names = enumerate names
-# ID drink = enumerate drinks
-# ID both to print selection for i.e. 
-#    -- for person, drink in zip(people, drinks)
-#    --     print(f"{enumnames} {enumdrinks}")
-
 # TODO: 6  Can you add drink preferences by ID of person/drink?
 # TODO: 7  Can you create a round? (round class)
 # TODO: 8  Are drink preferences persisted?
 # TODO: 9  Are round information persisted?
 # TODO: 11 Can the payer assign a round to themselves?
-# DONE: 12 Is there a sensible directory structure?
-# DONE: 13 Is data saving logic extracted to its own module?
 # TODO: 14 Are other classes extracted to their own module?
 # TODO: 15 Create Round, use ID for names, drinks in the order
 
@@ -22,9 +13,8 @@
 ### TODO: Assign a brewer to a Round
 ### TODO: Printing a Round
 
-# Do clear() after every command? Make it look "cleaner"
+# TODO: Do clear() after every command? Make it look "cleaner"
 
-# __init__(self, brewer, order={})
 
 # Separate Testing logic to new module
 # Make Python --m Unittest.py or something work. Test Suite
