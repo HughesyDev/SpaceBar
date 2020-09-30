0f9a6b85dbf140788258345862a50f16eb476e42
Jack <hughesy.dev@gmail.com>
2020-09-30 11:36:25 +0100

    Successfully performed db connection2

---
+++ db_testing/ETL_test.py
@@ -0,0 +1,13 @@
+import csv
+
+testdata = []
+
+ETL_CSV_FILEPATH = 'dbtesting/Import_User_Sample_en.csv'
+
+def load_csv():
+    try:
+        with open(ETL_CSV_FILEPATH, "r") as test_csv: 
+            testdata = test_csv.read().splitlines()
+    
+    except Exception as e:
+        print(f"Exception raised with the following error:\n {e}")
