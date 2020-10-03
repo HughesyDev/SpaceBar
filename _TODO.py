0729eec5ee114dc3adfdecad798b605ca7bbc047
Jack Hughes <hughesy.dev@gmail.com>
2020-10-03 18:15:14 +0100

    Amended TODO, changed to .py instead of .txt to aid with readability

---
+++ _TODO.py
@@ -0,0 +1,65 @@
+# DEMO:
+# 1. PRESENTATION
+#     - What challenges you faced
+#     - What you would develop if you had more time?
+#     - What 
+# 2. DEMO APP
+#     - Make sure it actually works beforehand, TEST that anything taking input accepts the correct input and handles invalid input.
+# 
+# # DEMO IS A WEEK ON TUESDAY to people from other cohort, Infinity Works people.
+# 
+# -----------------------
+# -----------------------
+# 
+# Look into UUID? Unique Identifier (low collision chance of hash thing?)
+# 
+# MUST: 
+# -----------------------
+''' DONE: Adding to people table'''
+''' DONE: Adding to drinks table'''
+# TODO BRANCH: Creating Preferences table and loading it into app
+# TODO BRANCH: Allow user to set their drink preference by name_id & drink_id
+''' DONE: Amend people table to accept firstname and surname.'''
+# 
+# ## First Draft of your future Junior Data Engineer CV - make sure to look through CherryTree notes
+#  - "engineering secure solutions to data-related problems" etc.
+# 
+# 
+# 
+# SHOULD:
+# -----------------------
+# 1. BRANCH: Complete Round class setup (without db interaction)
+# 
+# 
+# 
+# COULD:
+# -----------------------
+# BRANCH: Allowing user to update their drink preference
+''' DONE: Stop user from adding duplicate entries to people'''
+# BRANCH: Stop user from adding duplicate entries to drinks
+# BRANCH: Persist Round information in the db
+# BRANCH: Unit testing 
+# BRANCH: Make intro words write onto the screen instead of appearing (so char by char display like they're typed)
+# BRANCH: ETL stuff (import from CSV into file in correct format)
+# 
+
+''' "SPECIAL SAUCE"
+ Cheryse's had nice printout of rounds, it had age/alcoholic drinks etc.
+ # What can you do for yours?
+'''
+# 
+# TODO: Does the drink table hold extra drink information such as temperature, milk percentage and quantity?	YES	NO'''	
+# 
+# 
+# - Testing Suite
+# - Use more OOP
+# - Generate Unique ID for their order
+# - Print a receipt.
+# 
+# 
+# Make Python --m Unittest.py or something work. Test Suit
+# 
+# EXTRA FEATURES
+# - Leaderboards
+# - Brew stats: most popular brew, brews per round etc.
+# - A Slack bot
