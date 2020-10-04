732cf443f46d6fdd547bb6ae59ae672103d12360
Jack Hughes <hughesy.dev@gmail.com>
2020-10-04 19:48:32 +0100

    updated TODO with tasks completed on PREFS branch, deleted prefs branch

--- _TODO.py
+++ _TODO.py
@@ -20,8 +20,8 @@
 ''' DONE: Reimplement people retrieval/adding via db'''
 ''' DONE: Reimplement drinks retrieval/adding via db'''
 ''' DONE: Add prefs column to people which is FK to drink_id''' # My GOD this took ages
-# TODO BRANCH: Reimplement pref adding via db
-# TODO BRANCH: Allow user to set their drink preference by name_id & drink_id
+''' DONE: Reimplement pref adding via db'''
+''' DONE: Allow user to set their drink preference by name_id & drink_id'''
 ''' DONE: Amend people table to accept firstname and surname.'''
 # TODO BRANCH: Amend fields for name/surname to be NOT NULL? avoid empty entries.
 # TODO: First Draft of JDE CV -  look through CherryTree notes - "engineering secure solutions to data-related problems" etc.
@@ -34,14 +34,14 @@
 # -----------------------
 
 # COULD:
-# TODO BRANCH: Allowing user to update their drink preference
+''' DONE: Allowing user to update their drink preference'''
 ''' DONE: Stop user from adding duplicate entries to people'''
 ''' DONE: Stop user from adding duplicate entries to drinks'''
 # TODO BRANCH: Persist Round information in the db (if not exists, create new table for each round with nameid, drinkid, save it)
 # TODO BRANCH: Unit testing 
 # TODO BRANCH: Make intro words write onto the screen instead of appearing (so char by char display like they're typed)
 # TODO BRANCH: ETL stuff (import from CSV into file in correct format)
-# TODO: Make Python --m Unittest.py or something work. Test Suit
+# TODO: Make Python --m Unittest.py or something work. Test Suite
 # TODO: If table empty (i.e. all items deleted), then recreate table so all new items start index at 1 from the new table
 # TODO: Does the drink table hold extra drink information such as temperature, milk percentage and quantity?	YES	NO'''	
 
