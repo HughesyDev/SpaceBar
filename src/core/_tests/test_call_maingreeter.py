00d8aa7bff8bb0175b353ff07ac8d5ba320b53b5
Jack Hughes <hughesy.dev@gmail.com>
2020-10-07 16:59:23 +0100

    Disentangled broken test files from the main app. changed to txt so they're not picked up by pytest for now.

---
+++ src/core/_tests/test_call_maingreeter.py
@@ -0,0 +1,18 @@
+import unittest
+from unittest.mock import patch
+from unittest.mock import Mock
+from src.core.formatting.formatting_funcs import print_line
+
+class Test_Formatting(unittest.TestCase):
+    @patch("src.core.formatting.formatting_funcs.print_line")
+    @patch("builtins.print")
+    def test_print_line_returns_valid_width(self, print_mock, print_line_mock):
+        # ARRANGE
+        TEST_WIDTH = 20
+        EXPECTED = print_mock(f"+" + "=" * 20 + "+")
+
+        # ACT
+        ACTUAL   = print_line_mock(TEST_WIDTH)
+        
+        # ASSERT
+        return self.assertTrue(ACTUAL, EXPECTED)
