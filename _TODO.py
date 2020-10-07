61968fae297a7607d47d6486e5d7d9f71e042732
Jack Hughes <hughesy.dev@gmail.com>
2020-10-07 18:11:58 +0100

    Amended and re-arranged order of TODO list tasks.

--- _TODO.py
+++ _TODO.py
@@ -1,12 +1,14 @@
-# DEMO:
-# 1. PRESENTATION
-#     - What challenges you faced
-#     - What you would develop if you had more time?
-#     - What 
-# 2. DEMO APP
-#     - Make sure it actually works beforehand, TEST that anything taking input accepts the correct input and handles invalid input.
+'''
+DEMO:
+1. PRESENTATION
+    - What challenges you faced
+    - What you would develop if you had more time?
+    - What 
+2. DEMO APP
+    - Make sure it actually works beforehand, TEST that anything taking input accepts the correct input and handles invalid input.
+'''
 
-# Look into UUID? Unique Identifier (low collision chance of hash thing?)
+# Upload Prezi slide to LinkedIn / 
 
 # TODO: Change "Add drink" to "check if a drink you like is in stock."
 # TODO: Check what user added against a SET of many more drinks. If in stock: return it, add to db, load from db.
@@ -16,45 +18,43 @@
 
 # TODO: Create Drinks class? Subclass for hot / cold? (STRETCH goal)
 
-# Upload Prezi slide to LinkedIn / 
 
-# MUST: 
+
+#~~~~~~~~~~ MUST: 
 ''' DONE: Reimplement people retrieval/adding via db'''
 ''' DONE: Reimplement drinks retrieval/adding via db'''
 ''' DONE: Add prefs column to people which is FK to drink_id''' # My GOD this took ages
 ''' DONE: Reimplement pref adding via db'''
 ''' DONE: Allow user to set their drink preference by name_id & drink_id'''
 ''' DONE: Amend people table to accept firstname and surname.'''
-# TODO BRANCH: Add validation for adding people/drinks to catch empty strings.
+''' DONE: Add validation for adding people/drinks to catch empty strings.'''
 ''' DONE: First Draft of JDE CV -  look through CherryTree notes'''
-# TODO: Add an actual proper list of drinks.
-# TODO: Print Prefs: fix formatting.
+''' DONE: BRANCH: Add an actual proper list of drinks (Coke, Lemonade, Water)'''
+# TODO BRANCH: Print Prefs: fix formatting.
 
-########################## Python recognises empty strings as False
-########################## so do: def func(x) return x is False
-#################### Ternary: add_to_db if func(x) else print("Empty strings not accepted.")
-
-# -----------------------
-
-# SHOULD:
+#~~~~~~~~~~ SHOULD:
 # TODO BRANCH: Complete Round class setup (without db interaction)
 
-# -----------------------
-
-# COULD:
+#~~~~~~~~~~ COULD:
 ''' DONE: Allowing user to update their drink preference'''
 ''' DONE: Stop user from adding duplicate entries to people'''
 ''' DONE: Stop user from adding duplicate entries to drinks'''
-# TODO BRANCH: Persist Round information in the db (if not exists, create new table for each round with nameid, drinkid, save it)
-# TODO BRANCH: Unit testing 
-# TODO BRANCH: Make intro words write onto the screen instead of appearing (so char by char display like they're typed)
-# TODO: Make Python --m Unittest.py or something work. Test Suite
-# TODO: Does the drink table hold extra drink information such as temperature, milk percentage and quantity?	YES	NO'''	
 
+# TODO BRANCH: Persist Round information in the db (if not exists, create new table for each round with nameid, drinkid, save it)
+# TODO BRANCH: Unit testing / Pytest
+# TODO BRANCH: Make intro words write onto the screen instead of appearing (so char by char display like they're typed)
+# TODO BRANCH: Does the drink table hold extra drink information such as temperature, milk percentage and quantity?	YES	NO'''	
+# TODO BRANCH: create a function that rips down db tables and recreates to make dev easier
 
 # TODO BRANCH: ETL stuff (import from CSV into file in correct format)
 # TODO: If table empty (i.e. all items deleted), then recreate table so all new items start index at 1 from the new table
 
+########################## Python recognises empty strings as False
+########################## so do: def func(x) return x is False
+#################### ternary(?): add_to_db if func(x) else print("Empty strings not accepted.")
+
+
+# Look into UUID? Unique Identifier (low collision chance of hash thing?)
 
 # - Testing Suite
 # - Use more OOP
