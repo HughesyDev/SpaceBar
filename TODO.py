880d1fd2a027a83e96872a575d22b6a2796634d4
Jack Hughes <hughesy.dev@gmail.com>
2020-10-11 19:38:13 +0100

    amended TODO list, ticked off Round task and add more granular round-related future tasks.

--- TODO.py
+++ TODO.py
@@ -1,24 +1,4 @@
-'''
-DEMO:
-1. PRESENTATION
-    - What challenges you faced
-    - What you would develop if you had more time?
-    - What 
-2. DEMO APP
-    - Make sure it actually works beforehand, TEST that anything taking input accepts the correct input and handles invalid input.
-'''
-
 # Upload Prezi slide to LinkedIn ?
-
-# TODO: Change "Add drink" to "check if a drink you like is in stock."
-# TODO: Check what user added against a SET of many more drinks. If in stock: return it, add to db, load from db.
-# TODO: Add column for hot/cold drinks
-# TODO: Ask user if they want to see hot drinks or cold drinks table.
-# TODO: Hot drinks also have additional properties: milk, sugar, complementary biscuit.
-
-# TODO: Create Drinks class? Subclass for hot / cold? (STRETCH goal)
-
-
 
 #~~~~~~~~~~ MUST: 
 ''' DONE: Reimplement people retrieval/adding via db'''
@@ -30,30 +10,48 @@ DEMO:
 ''' DONE: Add validation for adding people/drinks to catch empty strings.'''
 ''' DONE: First Draft of JDE CV -  look through CherryTree notes'''
 ''' DONE: BRANCH: Add an actual proper list of drinks (Coke, Lemonade, Water)'''
-# TODO BRANCH: Print Prefs: fix formatting.
-# TODO BRANCH: Refactor menu_response code, change from if/else to i.e. a dict of {[text_string]: function_called}
+# TODO: Try to look for places where you can make the code more readable / "read like a story"
 
 #~~~~~~~~~~ SHOULD:
-# TODO BRANCH: Complete Round class setup (without db interaction)
+''' DONE: Reimplement Round class setup (without db interaction)'''
+# TODO BRANCH: Round: add receipt output
+# TODO BRANCH: Round: set favourite as default
+# TODO BRANCH: Print Prefs/order details: fix formatting to take in two columns of data.
+        # Unpacking things you send through to menu
+        # then iterating over the unpacked data into to fields, then width etc. for them.
+# TODO BRANCH: Refactor menu_response code
+        # Change from if/else to i.e. a dict of {[text_string]: function_called}
+# TODO: Avoid SQL injection by implementing 
 
 #~~~~~~~~~~ COULD:
 ''' DONE: Allowing user to update their drink preference'''
 ''' DONE: Stop user from adding duplicate entries to people'''
 ''' DONE: Stop user from adding duplicate entries to drinks'''
 
-# TODO BRANCH: Persist Round information in the db (if not exists, create new table for each round with nameid, drinkid, save it)
+# TODO BRANCH: Persist Round information in the db (
+        # Round table
+        # Give each instance of Round a UniqueID ("bill_payer" + datetime of instance initialisation)
+        # Then add the roundID, name, drink to table
+        # Keep track of each unique instance ID
+
 # TODO BRANCH: Unit testing / Pytest
+# TODO: Change "Add drink" to "check if a drink you like is in stock."
+        # Check what user added against a SET of many more drinks. If in stock: return it, add to db, load from db.
 # TODO BRANCH: Make intro words write onto the screen instead of appearing (so char by char display like they're typed)
 # TODO BRANCH: Does the drink table hold extra drink information such as temperature, milk percentage and quantity?	YES	NO'''	
 # TODO BRANCH: create a function that rips down db tables and recreates to make dev easier
 
+# TODO: Create Drinks class? Subclass for hot / cold? (STRETCH goal)
+        # Add column for hot/cold drinks
+        # Ask user if they want to see hot drinks or cold drinks table.
+        # Hot drinks also have additional properties: milk, sugar, complementary biscuit.
+
 # TODO BRANCH: ETL stuff (import from CSV into file in correct format)
 # TODO: If table empty (i.e. all items deleted), then recreate table so all new items start index at 1 from the new table
 
-########################## Python recognises empty strings as False
-########################## so do: def func(x) return x is False
-#################### ternary(?): add_to_db if func(x) else print("Empty strings not accepted.")
 
+
+# ~~~~~~~~~~ 
 
 # Look into UUID? Unique Identifier (low collision chance of hash thing?)
 
